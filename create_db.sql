1dda7a936f31a0a4a216eac3b9253fd7df6ff88e
Yunxi-Zhou <zhouyunxi1115@gmail.com>
2025-05-20 16:31:15 -0400

    add default setting and the basic operation of table

---
+++ create_db.sql
@@ -0,0 +1,7 @@
+create database sample_db character set gbk;
+drop database sample_db;  -- delete the whole table
+show databases;
+use sample_db;
+show tables;  -- show the col names of the table
+describe sample_db; -- show the structure of the table
+delete from sample_db; -- clear the record of the table
